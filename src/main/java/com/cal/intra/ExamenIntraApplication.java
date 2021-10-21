ad4663667e6f958d3f19cf0b7a9fa0d27cfefbbf
OussaKab <oussamakably@gmail.com>
2021-10-21 15:17:25 -0400

    premier push

---
+++ src/main/java/com/cal/intra/ExamenIntraApplication.java
@@ -0,0 +1,13 @@
+package com.cal.intra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenIntraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenIntraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cal/intra/ExamenIntraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cal.intra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenIntraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
